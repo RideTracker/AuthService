1b801588690ee8367ad25ee80d02bec1d53ef5f3
Nora Söderlund <norasoderlund@icloud.com>
2023-07-30 11:34:56 +0200

    Updated device token type

--- src/controllers/tokens/getTokenByKey.ts
+++ src/controllers/tokens/getTokenByKey.ts
@@ -1,10 +1,5 @@
 import { Token } from "../../models/Token";
 
 export async function getTokenByKey(database: D1Database, key: string): Promise<Token> {
-    return await database.prepare(
-        "SELECT tokens.*, users.email FROM tokens " +
-        "LEFT JOIN users ON users.id = tokens.user AND tokens.type = 'user' " +
-        "LEFT JOIN users ON users.id = (SELECT user FROM devices WHERE devices.id = tokens.user) AND tokens.type = 'device' " +
-        "WHERE tokens.key = ?"
-    ).bind(key).first();
+    return await database.prepare("SELECT tokens.*, users.email FROM tokens LEFT JOIN users ON users.id = tokens.user WHERE tokens.key = ?").bind(key).first();
 };
